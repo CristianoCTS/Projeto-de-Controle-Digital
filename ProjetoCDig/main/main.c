d19b07796d2c1099842b7590c032d3d78874dc46
Cristiano <cristolsan.cts.droid@droidunb.com>
2026-06-27 18:24:51 -0300

    tentando implementar

--- ProjetoCDig/main/main.c
+++ ProjetoCDig/main/main.c
@@ -5,62 +5,65 @@
 #include "esp_adc/adc_oneshot.h"
 #include "esp_timer.h"
 #include <stdint.h>
-
+#include <stdbool.h>
 
 // Definições de Hardware
 #define PWM_Heater 10
 #define ADC_UNIT ADC_UNIT_1
-#define ADC_Heater ADC_CHANNEL_0 
-#define ADC_Ambient ADC_CHANNEL_1 
+#define ADC_Heater ADC_CHANNEL_0
+#define ADC_Ambient ADC_CHANNEL_1
 #define Set_Heater(Value) ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, Value, 0)
 
-// Seed for random
-static uint16_t lfsr = 0xACE1u;
+// Parâmetros
+#define Sample_Period 1000   // ms
+#define PO            60.0f  // Ponto de operação °C
+#define PWM_Base      150
+#define PWM_Var       40
+#define MEDIAN_SIZE   5
+#define ALPHA         0.15f
 
-// Parametros
-#define Sample_Period 1000  
-#define PWM_Base 150   
-#define PWM_Var 40
-#define MEDIAN_SIZE 5
-#define KP 0.03f
-#define KI (KP * 17.0f)
-#define SATmax 12.0f
-#define SATmin 0.0f
-#define Smplg 1.0f
-#define ALPHA 0.15f
+// Saturação PWM
+#define USmax  1023.0f
+#define USmin  0.0f
 
+// Controlador
+#define K1   0.0213f
+#define K2   0.4825f
+#define KI_c 0.1017f
+#define NB   27.492f
+
+// Observador aumentado
+// L = [58.4773; 18.2415; 3.9058; -0.6964]
+#define L1  58.4773f
+#define L2  18.2415f
+#define L3   3.9058f
+#define L4  -0.6964f
+
+// Matriz [x1e; x2e; xs1; xs2]
+// Ad (4x4), Bd (4x1), Cd (1x4)
+static const float Ad[4][4] = {
+    { 0.9975f,  0.0f,    0.2351f,  0.1177f },
+    { 0.0f,     0.9782f, 0.2328f,  0.1169f },
+    { 0.0f,     0.0f,    0.9952f,  0.9984f },
+    { 0.0f,     0.0f,   -0.0096f,  0.9952f }
+};
+static const float Bd[4] = { 0.2354f, 0.2331f, 0.0f, 0.0f };
+static const float Cd[4] = { 0.0007f, 0.0158f, 0.0f, 0.0f };
+
+// Filtros
 static adc_oneshot_unit_handle_t adc_handle;
 static float median_sorted[MEDIAN_SIZE];
 
-typedef struct {
-    float integrador;
-} PIController;
 typedef struct {
     float buffer[MEDIAN_SIZE];
     uint8_t index;
     uint8_t count;
 } MedianFilter;
+
 typedef struct {
     float value;
     bool initialized;
 } EWMFilter;
-
-//Funções
-float PIcontrol (PIController *pi, float erro) {
-    float p_out = KP * erro;
-    float u_raw = p_out + (*pi).integrador;
-    bool clamping = ((erro > 0) && (u_raw > SATmax)) || ((erro < 0) && (u_raw < SATmin));
-    if (!clamping) {
-        (*pi).integrador += KI * Smplg * erro;
-    }
-    if (u_raw > SATmax){
-        u_raw = SATmax;
-    } 
-    else if (u_raw < SATmin){
-        u_raw = SATmin;
-    }
-    return u_raw;
-}
 
 float median_update(MedianFilter *f, float new_val) {
     f->buffer[f->index] = new_val;
@@ -88,14 +91,8 @@ float ewm_update(EWMFilter *f, float new_val) {
     return f->value;
 }
 
-uint8_t gerar_prbs_1_15(void) {
-    uint16_t bit = ((lfsr >> 0) ^ (lfsr >> 1) ^ (lfsr >> 3) ^ (lfsr >> 12)) & 1u;
-    lfsr = (lfsr >> 1) | (bit << 15);
-    return (lfsr & 0x0F) + 1;
-}
-
-void init_pwm() {
-
+// Configurações hardware
+void init_pwm(void) {
     ledc_timer_config_t ledc_timer = {
         .speed_mode = LEDC_LOW_SPEED_MODE,
         .timer_num = LEDC_TIMER_0,
@@ -115,14 +112,11 @@ void init_pwm() {
         .hpoint = 0
     };
     ledc_channel_config(&ledc_channel);
-
     ledc_fade_func_install(0);
 }
 
-void init_adc_oneshot() {
-    adc_oneshot_unit_init_cfg_t init_config = {
-        .unit_id = ADC_UNIT,
-    };
+void init_adc_oneshot(void) {
+    adc_oneshot_unit_init_cfg_t init_config = { .unit_id = ADC_UNIT };
     adc_oneshot_new_unit(&init_config, &adc_handle);
 
     adc_oneshot_chan_cfg_t config = {
@@ -136,66 +130,76 @@ void init_adc_oneshot() {
 float ADC_Read(adc_channel_t channel) {
     int raw;
     adc_oneshot_read(adc_handle, channel, &raw);
-    
     float voltage = (raw * 3.3f) / 4095.0f;
     return voltage / 0.01f;
 }
 
+//main
 void app_main(void) {
     init_pwm();
     init_adc_oneshot();
-    bool Switch = false;
-    uint32_t PWM_Value = (PWM_Base - PWM_Var);
-    int64_t last_time = -1;
-    printf("| Tempo (s) | PWM | Tensão(V) | Heater Normalizado(°C) | Ambient Normalizado(°C) | Heater(°C) | Ambient(°C) | Temp Alvo(°C) | Erro(°C) |\n");
-    uint8_t interval = gerar_prbs_1_15();
-    uint8_t last_interval = 0;
-    int64_t Last_impulse_time = 0;
 
     MedianFilter mf_heater = {0}, mf_ambient = {0};
     EWMFilter    ef_heater = {0}, ef_ambient = {0};
 
-    while (1) {
-        
-        int64_t Time = esp_timer_get_time()/1000;
-        int64_t impulse_time = Time/60000;
-        
-        if (Time != last_time) {
-            if (Time % (60000) == 0)
-            {
-                if ((impulse_time - Last_impulse_time) == interval) {
-                    Last_impulse_time = impulse_time;
-                    last_interval = interval;
-                    Switch = !Switch;
-                    PWM_Value = Switch ? (PWM_Base + PWM_Var) : (PWM_Base - PWM_Var);
-                    Set_Heater(PWM_Value);
-                    interval = gerar_prbs_1_15();
-                    while (abs(interval - last_interval) <= 1){
-                        interval = gerar_prbs_1_15();
-                    }
-                }
-            } 
-                    
-            if (Time % Sample_Period == 0)
-            {
-                float raw_H = ADC_Read(ADC_Heater);
-                float raw_A = ADC_Read(ADC_Ambient);
+    float xe[4] = {0.0f, 0.0f, 0.0f, 0.0f};
+    float xi = 0.0f;
+    float u_prev = 0.0f;
 
-                float Temp_H = ewm_update(&ef_heater,  median_update(&mf_heater,  raw_H));
-                float Temp_A = ewm_update(&ef_ambient, median_update(&mf_ambient, raw_A));
-                float Volts = (PWM_Value*0.008f);
-                float Target = (PWM_Value*0.233f) + Temp_A;
-                float Error = Temp_H - Target;
-                printf("-| %lld | %lu | %.2f | %.2f | %.2f | %.2f | %.2f | %.2f | %.2f |\n", (Time/1000), PWM_Value, Volts, Temp_H, Temp_A, raw_H, raw_A, Target, Error);
+    int64_t last_time = -1;
+
+    printf("| Tempo(s) | PWM | Us | Ye | e_sin | Temp_H | Temp_A | Ref | Erro |\n");
+
+    while (1) {
+        int64_t Time = esp_timer_get_time() / 1000;
+
+        if (Time != last_time && Time % Sample_Period == 0) {
+            float raw_H = ADC_Read(ADC_Heater);
+            float raw_A = ADC_Read(ADC_Ambient);
+            float Temp_H = ewm_update(&ef_heater,  median_update(&mf_heater,  raw_H));
+            float Temp_A = ewm_update(&ef_ambient, median_update(&mf_ambient, raw_A));
+            float Ye = Cd[0]*xe[0] + Cd[1]*xe[1] + Cd[2]*xe[2] + Cd[3]*xe[3];
+            float innov = Temp_H - PO - Ye;
+            float e_sin = xe[2];
+            float Kxe = K1*xe[0] + K2*xe[1];
+            float er = PO - Temp_H;
+            float Us = (NB*PO + KI_c*xi) - Kxe - e_sin;
+            float xe_new[4];
+            bool clamping = ((er > 0.0f) && (Us > USmax)) || ((er < 0.0f) && (Us< USmin));
+
+            if (!clamping) {
+                xi += er;
             }
+            if (Us > USmax) {
+                Us = USmax;
+            }
+            else if (Us < USmin) {
+                Us = USmin;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                xe_new[i] = Ad[i][0]*xe[0] + Ad[i][1]*xe[1] + Ad[i][2]*xe[2] + Ad[i][3]*xe[3] + Bd[i]*u_prev;
+            }
+            xe_new[0] += L1 * innov;
+            xe_new[1] += L2 * innov;
+            xe_new[2] += L3 * innov;
+            xe_new[3] += L4 * innov;
+
+            for (int i = 0; i < 4; i++) {
+                xe[i] = xe_new[i];
+            }
+
+            Set_Heater(Us);
+            u_prev = Us;
+
+            printf("| %lld | %lu | %.2f | %.2f | %.2f | %.2f | %.2f | %.2f | %.2f |\n",
+                   (Time/1000), PWM_Value, Us, Ye, e_sin, Temp_H, Temp_A, PO, er);
 
             last_time = Time;
         }
-        if (Time % Sample_Period == 3)
-        {
+
+        if (Time % Sample_Period == 3) {
             vTaskDelay(1);
         }
-        
-
     }
 }
